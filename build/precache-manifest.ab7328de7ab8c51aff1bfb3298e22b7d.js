bc4d935f20b0e005c8ba5f406d9cbbb5b1ea28c0
fornos.cuni.oriol <fornos.cuni.oriol@alumnat.copernic.cat>
2021-03-23 12:40:41 +0100

    prueba

---
+++ build/precache-manifest.ab7328de7ab8c51aff1bfb3298e22b7d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8ad4e12aeb1d65d71a9e38ce8f6300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2bd94baf15ae432ce58",
+    "url": "/static/js/2.b464b179.chunk.js"
+  },
+  {
+    "revision": "95837a46d0cb7f4fc8cb",
+    "url": "/static/js/main.f5e85005.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import VideoConference from "./VideoConference";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <VideoConference />
+  </React.StrictMode>,
+  rootElement
+);
